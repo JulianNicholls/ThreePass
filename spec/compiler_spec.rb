bff3b931607d727348e821c87a6d8f2c6c74c227
Julian Nicholls <juliannicholls29@gmail.com>
2013-10-26 20:47:55 +0100

    Added many tests

--- spec/compiler_spec.rb
+++ spec/compiler_spec.rb
@@ -1,5 +1,10 @@
 require_relative '../compiler'
 
+class Compiler
+  attr_reader :ast
+end
+
+
 describe Compiler do
 
   before do
@@ -7,16 +12,54 @@ describe Compiler do
   end
 
   describe "#pass1" do
-    it "should throw an error if there's no program" do
-      expect { @compiler.pass1 }.to raise_error( ParseError )
+    before do
+      @argx = {op: :arg, n: "x"}
+      @argy = {op: :arg, n: "y"}
+      @argz = {op: :arg, n: "z"}
+    end
+      
+    describe "Exceptions" do
+      it "should throw an error if there's no program" do
+        expect { @compiler.pass1 }.to raise_error( ParseError )
+      end
+      
+      it "should raise an error if the [ is missing" do
+        expect { @compiler.pass1 'a b c ] a+b+c' }.to raise_error( ParseError )
+      end
+      
+      it "should raise an error if the ] is missing" do
+        expect { @compiler.pass1 '[a b c a+b+c' }.to raise_error( ParseError )
+      end
     end
     
-    it "should raise an error if the [ is missing" do
-      expect { @compiler.pass1 'a b c ] a+b+c' }.to raise_error( ParseError )
+    describe "Simple Parsing" do
+      it "should parse a simple + operation" do
+        @compiler.pass1 '[x y] x + y'
+        expect( @compiler.ast ).to eq( {op: "+", a: @argx, b: @argy} )
+      end
+      
+      it "should parse a simple - operation" do
+        @compiler.pass1 '[x y] x - y'
+        expect( @compiler.ast ).to eq( {op: "-", a: @argx, b: @argy} )
+      end
+      
+      it "should parse a simple * operation" do
+        @compiler.pass1 '[x y] x * y'
+        expect( @compiler.ast ).to eq( {op: "*", a: @argx, b: @argy} )
+      end
+      
+      it "should parse a simple / operation" do
+        @compiler.pass1 '[x y] x / y'
+        expect( @compiler.ast ).to eq( {op: "/", a: @argx, b: @argy} )
+      end
     end
     
-    it "should raise an error if the ] is missing" do
-      expect { @compiler.pass1 '[a b c a+b+c' }.to raise_error( ParseError )
+    describe "Left Associativity" do
+      it "should work for a x + y + z operation" do
+        @compiler.pass1 '[x y z] x + y + z'
+        expect( @compiler.ast ).to eq( {:op => "+", :a => {:op => "+", :a => @argx, :b => @argy}, :b => @argz} )
+      end
     end
   end
+  
 end
