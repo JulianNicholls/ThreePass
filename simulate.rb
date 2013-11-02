ff3400e7d054fcbf99530e5cf293e6a59a8cc05a
Julian Nicholls <juliannicholls29@gmail.com>
2013-11-02 23:00:57 +0000

    Fixed matching and made values explicitly integer

--- simulate.rb
+++ simulate.rb
@@ -1,23 +1,35 @@
-def simulate(asm, args)
-  r0, r1 = 0, 0
-  stack  = [];
-  
-  asm.each do |instruct|
-    match  = instruct.match /(IM|AR)\s+(\d+)/ || [0, instruct, 0]
-    ins, n = match[1], match[2];
-    
-    case ins
-      when 'IM' then r0 = n
-      when 'AR' then r0 = args[n]
-      when 'SW' then r0, r1 = r1, r0
-      when 'PU' then stack.push r0
-      when 'PO' then r0 = stack.pop
-      when 'AD' then r0 += r1
-      when 'SU' then r0 -= r1
-      when 'MU' then r0 *= r1
-      when 'DI' then r0 /= r1
-    end
+class Simulator
+
+  def initialize( code, args )
+    set_code( code, args )
   end
   
-  r0
+  def set_code( code, args )
+    @code, @args = code, args
+  end
+  
+  def run
+    r0, r1 = 0, 0
+    stack  = [];
+    
+    @code.each do |instruct|
+      match  = instruct.match( /(IM|AR)\s+(\d+)/ ) || [0, instruct, 0]
+      ins, n = match[1], match[2];
+      
+      case ins
+        when 'IM' then r0 = n.to_i
+        when 'AR' then r0 = @args[n.to_i]
+        when 'SW' then r0, r1 = r1, r0
+        when 'PU' then stack.push r0
+        when 'PO' then r0 = stack.pop
+        when 'AD' then r0 += r1
+        when 'SU' then r0 -= r1
+        when 'MU' then r0 *= r1
+        when 'DI' then r0 /= r1
+      end
+    end
+    
+    r0
+  end
+  
 end
