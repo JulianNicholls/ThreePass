f63e6df313bf14e33dc910a78bcf5a4f8c19822f
Julian Nicholls <juliannicholls29@gmail.com>
2013-11-05 11:15:46 +0000

    Renamed instructions

--- simulate.rb
+++ simulate.rb
@@ -7,27 +7,33 @@ class Simulator
   
   def set_code( code, args )
     @code, @args = code, args
+    raise ArgumentError.new( "Arguments must be provided" ) unless @args.length > 0
+    raise ArgumentError.new( "Arguments must all be numeric constants" ) unless @args.all? { |n| n.is_a? Fixnum }
+    
     pp @code if @verbose
   end
   
   def run
     r0, r1 = 0, 0
-    stack  = [];
+    stack  = []
     
     @code.each do |instruct|
-      match  = instruct.match( /(IM|AR)\s+(\d+)/ ) || [0, instruct, 0]
-      ins, n = match[1], match[2];
+      match  = instruct.match( /(LD.I|LD.M)\s+(\d+)/ ) || [0, instruct, 0]
+      ins, n = match[1], match[2]
       
       case ins
-        when 'IM' then r0 = n.to_i
-        when 'AR' then r0 = @args[n.to_i]
-        when 'SW' then r0, r1 = r1, r0
-        when 'PU' then stack.push r0
-        when 'PO' then r0 = stack.pop
-        when 'AD' then r0 += r1
-        when 'SU' then r0 -= r1
-        when 'MU' then r0 *= r1
-        when 'DI' then r0 /= r1
+        when 'LD.I' then r0 = n.to_i
+        when 'LD.M' then r0 = @args[n.to_i]
+        when 'SWAP' then r0, r1 = r1, r0
+        when 'PUSH' then stack.push r0
+        when 'POP'  then r0 = stack.pop
+        when 'ADD'  then r0 += r1
+        when 'SUB'  then r0 -= r1
+        when 'MUL'  then r0 *= r1
+        when 'DIV'  then r0 /= r1
+        
+        else
+          raise ArgumentError.new( "Unknown: #{ins}" )
       end
     end
     
